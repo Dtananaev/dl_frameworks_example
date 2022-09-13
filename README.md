cd28031902f1252352007eb2adafaa4d97f6f17e
Denis Tananaev <d.d.tananaev@gmail.com>
2022-09-13 13:18:40 +0200

    added readme installation instruction

--- README.md
+++ README.md
@@ -8,16 +8,12 @@ The example is done on binary semantic segmentation dataset.
 Create workspace folder in home directory
 ```
 mkdir ~/deeplearning_workspace
-
 ```
 and clone your repository to that folder.
 ```
 git clone https://github.com/Dtananaev/dl_frameworks_example.git
 cd ~/deeplearning_workspace
-
 ```
-
-
 
 ## Install docker
 
@@ -29,9 +25,44 @@ bash ~/deeplearning_workspace/frameworks_example/docker/build_image.sh
 ```
 The docker should be build successfully.
 
+`Note: docker has root access!`
+
 ## Seting up virtual environment
 
 1. Create container bashrc file with following command:
 ```
-echo "source ~/deeplearning_workspace/venv/dl_env/bin/activate" > ~/deeplearning_workspace/.bashrc
+echo "source ~/dl_env/bin/activate" > ~/deeplearning_workspace/.bashrc
+```
+2. Activate docker:
+```
+bash ~/deeplearning_workspace/frameworks_example/docker/run_docker.sh
+```
+3. Create virtualenv:
+```
+python3 -m venv --system-site-packages ./dl_env
+```
+
+and activate
+```
+source ~/dl_env/bin/activate
+```
+and update pip tools:
+```
+pip install --upgrade pip
+```
+4. Install pytorch:
+```
+pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu113
+```
+5. Install tensorflow 2.0:
+```
+pip3 install tensorflow-gpu==2.5.0
+```
+6. Install requirements:
+```
+pip install -r dl_frameworks_example/requirements.txt
+```
+7. Install the package:
+```
+pip install -e dl_frameworks_example/
 ```
---
+++ framework_examples/create_dataset_list.py
@@ -0,0 +1,76 @@
+"""Creates dataset list."""
+__copyright__ = """
+Copyright (c) 2022 Tananaev Denis
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions: The above copyright notice and this permission
+notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+"""
+import os
+import glob
+import numpy as np
+import argparse
+from typing import List
+
+class CreateDataList:
+    """The class creates list of pairs image and label.
+    
+    Args:
+        dataset_dir: the directory with data
+    """
+    def __init__(self, dataset_dir: str)-> None:
+        """Initialize."""
+        self.dataset_dir = dataset_dir
+
+
+    def _get_data(self)-> List[str]:
+        """Gets data list from given dataset directory."""
+
+        search_string = os.path.join(self.dataset_dir, "images", "*.png")
+        images_list = np.asarray(sorted(glob.glob(search_string)))
+
+        search_string = os.path.join(self.dataset_dir, "depth", "*.png")
+        depth_list = np.asarray(sorted(glob.glob(search_string)))
+
+        data = np.concatenate((images_list[:, None], depth_list[:, None],), axis=1,)
+        data = [";".join(x) for x in data]
+        return data
+
+    def create_datasets_file(self, split="train"):
+        """Creates dataset list file.
+        
+        Args:
+            split: train, val, test split
+        """
+        data_list = self._get_data()
+
+        # Save dataset
+        filename = os.path.join(self.dataset_dir, f"{split}.datatxt")
+
+        with open(filename, "w") as f:
+            for item in data_list:
+                f.write("%s\n" % item)
+        print(
+            f"The dataset of the size {len(data_list)} saved in {filename}."
+        )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Create dataset file.")
+    parser.add_argument("--dataset_dir", default="../dataset")
+    args = parser.parse_args()
+    dataset_creator = CreateDataList(args.dataset_dir)
+
+    splits = ["train", "val", "test"]
+    for split in splits:
+        # Note for demo purposes all splits contain the same data
+        dataset_creator.create_datasets_file(split=split)
